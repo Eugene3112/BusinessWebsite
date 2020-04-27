7e3e93eec1bb545f5214287dd42229997864e50f
Eugene3112 <bertoshevgieniy@gmail.com>
2020-04-27 13:49:02 +0300

    add: main block

--- js/main.js
+++ js/main.js
@@ -5,9 +5,16 @@ $(document).ready(function(){
         speed: 300,
         slidesToShow: 3,
         slidesToScroll: 3,
-        responsive: [
-            {
-            breakpoint: 790,
+        responsive: [{
+                breakpoint: 800,
+                settings: {
+                    dots: true,
+                    slidesToShow: 2,
+                    slidesToScroll: 1
+                }
+                },
+                {
+            breakpoint: 650,
             settings: {
                 dots: true,
                 slidesToShow: 1,
